57682f3d8998133cdd56d786297c35bd896c56bd
KhushiSaini7 <94100598+KhushiSaini7@users.noreply.github.com>
2023-07-11 06:12:59 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import React, {useState, useMemo} from 'react'
+import styled from "styled-components";
+import bg from './img/bg.png';
+import { MainLayout } from "./styles/Layouts";
+import Orb from "./Components/Orb/Orb";
+import Navigation from './Components/Navigation/Navigation';
+
+function App(){
+    const [active, setActive] = useState(1);
+
+    const orbMemo = useMemo(() => {
+        return <Orb />
+      },[])
+
+    return(
+      <AppStyled bg={bg} className="App"  >
+        {orbMemo}
+        <MainLayout>
+        <Navigation active={active} setActive={setActive} />
+          
+        </MainLayout>
+      </AppStyled>
+    ); 
+}
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${props=>props.bg});
+  position: relative;
+`;
+
+export default App;
